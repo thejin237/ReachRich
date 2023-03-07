3795d70ce34304e10e5a810f1a5e56274e1276da
thejin237 <thejin237@gmail.com>
2023-03-07 21:10:12 +0900

    [Create]ReachRich Project

---
+++ ReachRich/src/test/java/com/reachrich/ReachRichApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reachrich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReachRichApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
